6c70a4a49626c52e6547eeb83e57b26739603c90
Aaron Grabowy <8436565+detzt@users.noreply.github.com>
2024-09-02 21:02:01 +0200

    Setup for #12 Not possible to expand & collapse a DropdownField by script

---
+++ Assets/Scripts/DropdownController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DropdownController : MonoBehaviour
+{
+    [SerializeField]
+    private UIDocument uIDocument;
+
+    protected void Start()
+    {
+        // Find the dropdown element
+        var dropdown = uIDocument.rootVisualElement.Q<DropdownField>();
+
+        // I can set available options
+        dropdown.choices = new() { "Option 1", "Option 2", "Option 3" };
+
+        // I can set the current value
+        dropdown.index = 1;
+
+        // But I cannot expand or collapse the dropdown
+    }
+}

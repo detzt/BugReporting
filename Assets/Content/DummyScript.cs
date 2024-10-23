09e73e0b773b523b91a9e0fee2e32ddcd77cb6db
Aaron Grabowy <8436565+detzt@users.noreply.github.com>
2024-10-23 20:52:33 +0200

    Setup to showcase searching for direct references

---
+++ Assets/Content/DummyScript.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour
+{
+    [SerializeField, Tooltip("Reference to the model object to create expandable foldout in search results")]
+    private GameObject model;
+}
